97904a9bc9f7643229688fa64a26de281cb638e9
DennisAyo <78748705+DennisAyo@users.noreply.github.com>
2021-09-07 00:43:52 -0500

    Código
    Eliminar alumno

--- Código/aplicacion/alumnodelete.php
+++ Código/aplicacion/alumnodelete.php
@@ -11,7 +11,7 @@ if($_SESSION['rol'] =='Administrador') {
             echo $e->getMessage();
         }
     } else {
-        die('Ha ocurrido un error en sistema');
+        die('Ha ocurrido un error');
     }
 }else{
     header('location:inicio.view.php?err=1');
